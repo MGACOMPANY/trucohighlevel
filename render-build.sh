29febd6ee68605b7585d8226983c69d17ff4a900
unknown <makegreeatagaingmail.com>
2025-04-18 00:42:11 -0300

    Remuevo dependencias innecesarias como sharp

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-# Descargar e instalar Chrome
-mkdir -p /opt/chrome
-curl -sSL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o chrome.deb
-dpkg -x chrome.deb /opt/chrome
-rm chrome.deb
-
-# Setear path para Puppeteer/WPPConnect
-echo "export PUPPETEER_EXECUTABLE_PATH=/opt/chrome/opt/google/chrome/google-chrome" >> $HOME/.bashrc
+apt-get update
+apt-get install -y wget gnupg
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+npm install
