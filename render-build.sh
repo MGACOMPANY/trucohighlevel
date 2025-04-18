476f5aa24fda32948675244c6a34f351cc8f8e4b
unknown <makegreeatagaingmail.com>
2025-04-17 23:07:27 -0300

    fix: configuración final para Puppeteer en Render

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Descargar e instalar Chromium
+# Descargar e instalar Chrome
 wget -q -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i chrome.deb || apt-get -f install -y
 rm chrome.deb
+
+# Establecer ruta del ejecutable para Puppeteer/WPPConnect
+export PUPPETEER_EXECUTABLE_PATH=$(which google-chrome)
